acb5bfc706a6e1e18b05302e536c5dd4722d05ed
suhas-teacher <70425284+suhas-teacher@users.noreply.github.com>
2021-07-26 13:45:51 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,33 @@
+var btn_red;
+var btn_green;
+
+var r = 0;
+var g = 0;
+var b = 0;
+
+function setup() {
+  createCanvas(400, 400);  
+  btn_red=createButton("RED");
+  btn_red.position(100,50);
+  btn_green=createButton("GREEN");
+  btn_green.position(200,50);
+  btn_red.mousePressed(redbg);
+  btn_green.mousePressed(greenbg);
+
+}
+
+function draw() {
+  background(r,g,b);
+  }
+
+function redbg(){
+  r=255;
+  g=0;
+  b=0;
+}
+
+function greenbg(){
+  r=0;
+  g=255;
+  b=0;
+}
